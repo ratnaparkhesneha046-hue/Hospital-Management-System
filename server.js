59cbeb1c188c37ae9255d128fa6379333b026d34
Sneha Ratnaparkhe <ratnaparkhesneha046@gmail.com>
2026-04-26 21:54:05 +0530

    added root route

--- server.js
+++ server.js
@@ -4,18 +4,21 @@ require('dotenv').config();
 
 const app = express();
 
-// 🔥 ADD THESE TWO LINES
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// Routes
+
 app.use('/patients', require('./routes/patients'));
 app.use('/doctors', require('./routes/doctor'));
 app.use('/appointments', require('./routes/appointments'));
 app.use('/billing', require('./routes/billing'));
 
+app.get("/", (req, res) => {
+  res.send("Hospital Management System Running 🚀");
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
